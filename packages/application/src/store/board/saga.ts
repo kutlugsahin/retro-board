599f77726c0ac4a9b470f8d9f31ad2f48d6d402a
Kutlu Sahin <kutlugsahin@gmail.com>
2019-11-17 21:22:33 +0100

    dsa

--- packages/application/src/store/board/saga.ts
+++ packages/application/src/store/board/saga.ts
@@ -1,20 +1,19 @@
 import { SagaIterator } from 'redux-saga';
 import { takeEvery, all, put } from '@redux-saga/core/effects';
 import { CREATE_BOARD, GET_BOARD, GetBoardAction, setBoard } from './actions';
-import { json } from 'express';
 
 function* createBoard() {
-    const board = yield fetch('/api/retro/create').then(p => json());
-	yield put(setBoard(board));
-	console.log(board);
-	window.history.pushState(null, '', `/${board.id}`);
+    const board = yield fetch('/api/retro/create').then(p => p.json());
+    yield put(setBoard(board));
+    console.log(board);
+    window.history.pushState(null, '', `/${board.id}`);
 }
 
 function* getBoard({ payload: { id } }: GetBoardAction) {
-    const board = yield fetch(`/api/retro/${id}`).then(p => json());
-	yield put(setBoard(board));
+    const board = yield fetch(`/api/retro/${id}`).then(p => p.json());
+    yield put(setBoard(board));
 }
 
-export const retroSaga = function*(): SagaIterator {
+export const retroSaga = function* (): SagaIterator {
     yield all([takeEvery(CREATE_BOARD, createBoard), takeEvery(GET_BOARD, getBoard)]);
 };
